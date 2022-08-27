cb069e7072506f210ba2aeb21b8aa26b7b652889
francoariel1492 <francoariel1492@gmail.com>
2022-08-27 12:46:45 +0200

    Desafio Incorporar arrays

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,54 +1,52 @@
 class Mueble{
-    constructor(tipo, largo, precio){
+    constructor(tipo, medida, precio){
         this.tipo = tipo;
-        this.largo = largo + "m ";
+        this.medida = medida + "m ";
         this.precio = "$" + precio;
+        precioTotal += precio;
     }
 }
 
+let muebles = [];
+
 let precioTotal = 0;
 
-lista = [];
-
+    
+function agregarMueble(muebles){
+    let medida = parseInt(prompt("Cuantos metros vas a llevar?"));
+    let precio = medida * valorMadera;
+    let madera = new Mueble(tipo, medida, precio);
+    alert("Agregaste " + madera.medida +" de " + madera.tipo + " serian " + madera.precio + " al changuito");
+    muebles.push(madera);
+}
 
 function comprar(){
-
+    
     while(true){
-        let madera = parseInt(prompt("Tenemos 3 opciones de madera para guitarras: \n1.Ebano, \n2.Aliso \n3.Nogal \n4.Ir a changuito \n5.Salir"));
+        let madera = 0;
+        madera = parseInt(prompt("Tenemos 3 opciones de madera para guitarras: \n1.Ebano, \n2.Aliso \n3.Nogal \n4.Ir a changuito \n5.Salir"));
         
         if(madera == 1 ){
-            alert("Ebano perfecto para una Les Paul al mejor estilo Jimmy Page!");
-            let cantidadUnidades = parseInt(prompt("Cuantos metros vas a llevar?"));
-            let precio = cantidadUnidades * 100;
-            const Ebano = new Mueble("Ebano", cantidadUnidades, precio);
-            alert("Agregaste " + Ebano.largo +" de Ebano, serian " + Ebano.precio + " al changuito" )
-            precioTotal += precio;
-            lista.push(Ebano);
+            tipo = "Ebano";
+            valorMadera = 100;
+            alert(tipo + " perfecto para una Les Paul al mejor estilo Jimmy Page!");
+            agregarMueble(muebles);
 
         } else if(madera == 2){
-
-            alert("Aliso es la madera para la legendaria Stratocaster, la de Jimi Hendrix!");
-            let cantidadUnidades = parseInt(prompt("Cuantos metros vas a llevar?"));
-            let precio = cantidadUnidades * 120;
-            const Aliso = new Mueble("Aliso", cantidadUnidades, precio);
-            alert("Agregaste " + Aliso.largo +" de Aliso, serian " + Aliso.precio + " al changuito" )
-            precioTotal += precio;
-            lista.push(Aliso);
-            
+            tipo = "Aliso";
+            valorMadera = 120;
+            alert(tipo + " es la madera para la legendaria Stratocaster, la de Jimi Hendrix!");
+            agregarMueble(muebles);
 
         } else if(madera == 3){
+            tipo = "Nogal";
+            valorMadera = 90;
+            alert("Con el " + tipo +  " podriamos hacer una Telecaster al estilo George Harrison que te parece?");
+            agregarMueble(muebles);
 
-            alert("Con el nogal podriamos hacer una Telecaster al estilo George Harrison que te parece?");
-            let cantidadUnidades = parseInt(prompt("Cuantos metros vas a llevar?"));
-            let precio = cantidadUnidades * 90;
-            const Nogal = new Mueble("Nogal", cantidadUnidades, precio);
-            alert("Agregaste " + Nogal.largo +" de Nogal, serian " + Nogal.precio + " al changuito" )
-            precioTotal += precio;
-            lista.push(Nogal);
-
-        }else if(madera == 4){
-            let changuito = lista.map((Mueble) => (Mueble.tipo + " " + Mueble.largo  + Mueble.precio + "\n"));
-            alert("Tu compra de: " + "\n\n" + changuito.join('') + "\nEs un total de $" +  precioTotal);
+        }else if(madera == 4 && madera != true){
+            let changuito = muebles.map((Mueble) => (Mueble.tipo + " " + Mueble.medida  + Mueble.precio + "\n"));
+            alert("Tu compra de:" + "\n\n" + changuito.join('--') + "\n" + "Es un total de: $" + precioTotal );
             pagoCuotas();
             break;
 
@@ -58,11 +56,11 @@ function comprar(){
         }
         else{
             alert("No es posible realizar la operacion");
-            break;
         }
         
     }
 }
+
 
 function pagoCuotas(){
     while(true){
@@ -73,7 +71,7 @@ function pagoCuotas(){
             totalCuotas = totalCuotas.toFixed(2);
             alert("Perfecto te quedarian en " + cuotas +" cuotas de " + totalCuotas + " nos contactaremos en cuanto tengamos tu pedido listo para retirar");
             break;
-        }else if(pregunta == "N"){
+        }else if(pregunta == "N") {
             alert("Perfecto el total es de: $" + precioTotal + " puedes pagarlo en efectivo en nuestro local fisico en Av Laprida 222 o realizar tu pago mediante tarjeta de credito/debito o mercadopago");
             break;
         }else{
